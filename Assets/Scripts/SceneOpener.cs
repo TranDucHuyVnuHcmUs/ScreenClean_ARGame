d012f90c178820f5a027ea0d148d0ae709f2b016
Trần Đức Huy <38009176+TranDucHuyVnuHcmUs@users.noreply.github.com>
2023-07-06 14:56:16 +0700

    Chores: UI, navigate through scenes

--- Assets/Scripts/SceneOpener.cs
+++ Assets/Scripts/SceneOpener.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour
+public class SceneOpener : MonoBehaviour
 {
+    public const string MAIN_MENU_SCENE_NAME = "MenuScene";
+    public const string GAME_SCENE = "GameScene";
     public const string RECORD_SCENE_NAME = "HandGestureRecorderScene";
     public const string BINDING_SCENE_NAME = "HandGestureBindingScene";
 
+    public void OpenMainMenuScene()
+    { SceneManager.LoadScene(MAIN_MENU_SCENE_NAME); }
+
     public void OpenGameScene()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(GAME_SCENE);
     }
 
     public void OpenRecordScene()
